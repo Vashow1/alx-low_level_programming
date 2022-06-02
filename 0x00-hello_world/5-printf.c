012072784cba11fd1a2b8b7b664707c9ddbfb4c6
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-02 15:56:55 -0700

    Updates

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - print generic statement
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	/*string initialisation*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
