7c77ecd80e28d0e6db9a676f8775ad220f4079cb
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-02 16:28:42 -0700

    Updates

---
+++ 0x00-hello_world/101.quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - print generic statement
+ *
+ * Return: 1 after printing the function
+ */
+int main(void)
+{
+	/*string initialisation*/
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
