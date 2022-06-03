e71de3ef5eb444c3ddc659330400b7da47acb931
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-03 13:02:52 -0700

    Updates

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,16 +12,16 @@ int main(void)
 	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int n = sizeof(alphabet);
 	int i = 0;
+	int j;
 
 	while (i < n)
 	{
 		putchar(alphabet[i]);
 		i++;
 	}
-	for (i = 0; i < n; i++)
+	for (j = 0; j < n; j++)
 	{
-		putchar(toupper(alphabet[i]));
-		i++;
+		putchar(toupper(alphabet[j]));
 	}
 	putchar('\n');
 	return (0);
