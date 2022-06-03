0bc23b6303f79a58b915b358d0e687ba6676d9b9
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-03 14:50:22 -0700

    Updates

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: 0 after looping through successfully
+ */
+int main(void)
+{
+	char digit = '0';
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
