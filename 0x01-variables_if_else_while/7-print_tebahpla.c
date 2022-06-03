955b1affeae22534d7cbbc8661fb973ae6ace39d
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-03 14:04:55 -0700

    Updates

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: 0 after looping through successfully
+ */
+int main(void)
+{
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+	int n = sizeof(alphabet) - 1;
+
+	while (n > 0)
+	{
+		putchar(alphabet[n]);
+		n--;
+	}
+	return (0);
+}
