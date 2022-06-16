e77c7ab565c2097ac804bd583e2ff76d982550c0
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-16 06:39:58 -0700

    Updates

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lower case ten times
+ *
+ * return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet = 'a';
+	int j = 1;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 int printstr(char sampleStr[], int sizeofString);
 void print_alphabet();
+void print_alphabet_x10(void);
