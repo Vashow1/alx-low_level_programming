8af2ab842360f7ac591e77265da84576c53b1f93
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-16 13:57:32 -0700

    Updates

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers and returns the result
+ * @a: integer got from main programme
+ * @b: integer also got from main programme
+ *
+ * Return: summation of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * return: void
+ */
+void times_table(void)
+{
+	int n;
+	int multiplier;
+	int product;
+
+	for (n = 0; n <= 9; num++)
+	{
+		_putchar('0');
+		for (multiplier = 1; multiplier <= 9; multiplier++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			product = n * multiplier;
+
+			if (product <= 9)
+			{
+				_putchar((prod / 10) + '0');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
