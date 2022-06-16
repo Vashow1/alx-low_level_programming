33433e755cbd0b26bfcc564eb86cc8252e6e54be
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-16 09:55:18 -0700

    Updates

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all the lower case letters
+ *
+ * return: void
+ */
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z))
+		return 1;
+	else
+		return 0;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 int printstr(char sampleStr[], int sizeofString);
 void print_alphabet();
 void print_alphabet_x10(void);
+int _islower(int c);
