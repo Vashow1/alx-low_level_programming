8311247381f65d37ac7fc6df76a514c51bb07c21
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-16 06:26:41 -0700

    Updates

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
 #include "main.h"
-void print_alphabet()
+/**
+ * print_alphabet - prints all the lower case letters
+ *
+ * return: void
+ */
+void print_alphabet(void)
 {
 	char alphabet = 'a';
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
 	_putchar('\n');
-}         
+
