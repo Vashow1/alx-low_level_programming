b470b2cde0fb4f7b3fb8baa0701599c950a755a6
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-16 20:59:30 -0700

    Updates

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,14 +5,15 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
+	int index;
 
-	while (s[index++])
-		len++;
+	len = sizeof(s);
 
-		for (index = len - 1; index >= 0; index--)
-			_putchar(s[index]);
-
+	for (index = len - 1; index >= 0; index--)
+	{
+		_putchar(s[index]);
+	}
 	_putchar('\n');
 }
 
