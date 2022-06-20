06a00886eaafcd8defc0a12dae1bad8ef9d20306
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-20 03:38:25 -0700

    Updates

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - function fills the first n bytes of the memory area,
  * pointed to by s with the constant byte b
- * @*s: memory area
+ * @s: memory area
  * @b: a constant with which the mem area will be filled
  * @n: the number of bytes to be filled after,
  * starting at the area pointed to by s
