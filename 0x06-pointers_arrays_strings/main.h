036ded0425901b657111f9ab0a01504a96cb91c9
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-20 13:20:03 +0300

    Updates

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# C - More pointers, arrays and strings
+
+In this directory contains the files necessary to complete the assignments in regards to the second Arrays pointers and strings assignments.
