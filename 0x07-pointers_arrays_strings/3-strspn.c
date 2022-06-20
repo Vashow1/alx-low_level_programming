041a8062da48f270e6d3aa8f4c21d285842f6b77
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-20 22:12:45 +0300

    Updates

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - Returns the number of bytes in the initial segment of s,
+ * which consist only of bytes from accept
+ * @s: string parsed to be worked on
+ * @accept: values to be searched for
+ * 
+ * Return: number of of occurences of accept values in s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+			}
+		}
+	}
+	
+	return (count);
+}
