b1b64266ae6baec4eb67b904e3564b24e45df9c8
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-21 12:34:43 +0300

    Updates

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,16 @@
 void print_chessboard(char (*a)[8])
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (i < 8)
 	{
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
+		
+		_putchar('\n');
 		i++;
 	}
 }
