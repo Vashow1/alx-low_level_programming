369dbcb9c2825210a0cc7d17b329b1e530e6d024
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-21 13:17:23 +0300

    Updates

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: a square matrix of integers
+ * @size: size of the matrix
+ */
+
+
+void print_diagsums(int *a, int size)
+{
+	int sumf = 0, sumr = 0, i = 0, j = 0;
+
+	while (i < size)
+	{
+		sumf += a[i];
+		a += size;
+		i++;
+	}
+
+	a -= size;
+	
+	while(j < size)
+	{
+		sumr += a[j];
+		a -= size;
+		j++;
+	}
+	
+	printf("%d, %d\n", sumf, sumr);
+}
