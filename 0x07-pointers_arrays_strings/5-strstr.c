da7c0b8d97b464c018504bd60a195460b4cfb6a0
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-21 11:55:26 +0300

    Updates

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -24,7 +24,7 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[i])
 		{
 			do {
-				if (*needle == '\0')
+				if (needle[i + 1] == '\0')
 				{
 					return (haystack);
 				}
