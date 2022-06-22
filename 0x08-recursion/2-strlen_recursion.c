18dd10753fa2c894df6ec804ab8eb19bce164f08
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-22 23:30:00 +0300

    Updates

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * s: the string to be parsed. 
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 1;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (count + _strlen_recursion(s + 1));
+}
