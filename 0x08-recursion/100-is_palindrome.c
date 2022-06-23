2f737fc9a4045496a98e10f20afa70f86cfc99f2
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-23 13:02:59 -0700

    Updates

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,6 +3,9 @@
 /**
  * is_palindrome - checks if a string is a palindrome or not.
  * @s: the string to be checked.
+ * The if condition checks if string is empty,
+ * and if it is, it is a palindrome by default
+ * it also acts as a check so as to not cause an infinite loop
  *
  * Return: 1 if palindrome and 0 if not.
  */
@@ -12,12 +15,6 @@ int is_palindrome(char *s)
 	int i = 0;
 	int len = length(s);
 
-/**
- * This condition checks if string is empty,
- * and if it is, it is a palindrome by default
- * it also acts as a check so as to not cause an infinite loop
- *
- */
 	if (!(*s))
 	{
 		return (1);
@@ -28,7 +25,8 @@ int is_palindrome(char *s)
 /**
  * length - computes the length of the string s.
  * @s: the string in question
- * return: the length of the string
+ *
+ * Return: the length of the string
  */
 
 int length(char *s)
@@ -44,13 +42,15 @@ int length(char *s)
 }
 
 /**
- * check_palindrome - takes in various variables and
+ * check_pali - takes in various variables and
  * checks if string is a palindrome or not.
  * @s: the string to be checked.
  * @len: the length of the string
  * @i: the index to keep track where the pointer is.
+ *
  * Return: 1 if palindrome and 0 if not.
  */
+
 int check_pali(char *s, int len, int i)
 {
 	if (s[i] == s[len / 2])
