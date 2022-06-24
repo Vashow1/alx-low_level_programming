5a556a0f78555f06eca67d4f706ed19af2ad03dd
Muriuki Wanjohi <vashow@students.uonbi.ac.ke>
2022-06-24 02:39:12 -0700

    Updates

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,4 +2,15 @@
 
 /**
  * main - prints the number of arguments passed into it
- * @
+ * @argc: variable containing number of arguments in command line
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 if successful.
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
