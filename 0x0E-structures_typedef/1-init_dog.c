0f154345edc6e7f735f0c4046eeafd32a9df03b0
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-06 01:46:09 +0300

    Updates

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: variable that is the struct itself
+ * @name: name of the struct variable 'dog'
+ * @age: age of the struct variable 'dog'
+ * @owner: string of the owner of the struct variable 'dog'
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints the variable values
+ * 
+ * @d: the struct dog being parsed.
+ */
+
+void print_dog(struct dog *d)
+{
+	(d->name == NULL) ? ("(nil)") : (d->name);
+	printf("Name: %s\n", d->name);
+	if (d->age == NULL)
+	{
+		printf("Age: (nil)\n", );
+	}
+	else
+	{
+		printf("Age: %i\n", d->age);
+	}
+	(d->owner == NULL) ? ("(nil)") : (d->owner);
+	printf("Owner: %s\n", d->owner);
+}
