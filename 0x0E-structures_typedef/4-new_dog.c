8c4591faaac1a23408c6069b3c59fbfdf6d3f2e6
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-07 13:22:07 +0300

    Updates

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog.
+ * @name: the name of the dog
+ * @age: age of the struct variable 'dog'
+ * @owner: the owner of the struct variable 'dog'
+ *
+ * Return: dog_t* 
+ */
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
