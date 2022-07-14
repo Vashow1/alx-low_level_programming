cf30d39b90449148fff62fd8aa7ee601dd107780
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-14 23:31:08 +0300

    Redoing...

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,14 +13,33 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *node = malloc(sizeof(list_t));
 	int len = 0;
+	char *nodestring = strdup(str);
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	if (str == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = node;
+	}
 
 	while (head)
 	{
 		len++;
+		if (head->next == NULL)
+		{
+			break;
+		}
 		head->next;
 	}
 	head->next = node;
 	node->len = len;
-	node->str = strdup(str);
+	node->str = ;
 	node->next = NULL;
 }
