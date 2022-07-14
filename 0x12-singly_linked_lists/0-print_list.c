1610dba88fe22601ac62013233a6bdeafdab3e43
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-14 21:04:02 +0300

    Creating files for Linked lists project

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ *
+ * h: the list.
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t no_node = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		no_node++;
+	}
+	return (no_node);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+# alx-low_level_programming
+This directory contains files that are related to learning Linked Lists.
