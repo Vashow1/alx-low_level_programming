0562678f726e4fe0aa7c6c6b276e638f00a53abb
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-16 17:23:00 +0300

    Fixing errors

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,9 +18,16 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	grid = malloc(sizeof(int) * height);
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
+
 		if (grid[i] == NULL)
 		{
 			for (; i >= height; i--)
@@ -38,6 +45,7 @@ int **alloc_grid(int width, int height)
 			grid[i][j] = 0;
 		}
 	}
+	
 	return (grid);
 
 }
