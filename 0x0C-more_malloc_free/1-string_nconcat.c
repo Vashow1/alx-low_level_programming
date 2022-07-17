21ab979840ddbe8bf3c99eb582f82b2d720e6e49
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-17 21:08:13 +0300

    Fixing string_nconcat file errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,37 +13,33 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *newString;
-	unsigned int index, index1, lenString;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (index = 0; s1[index]; index++)
-	{
-		lenString++;
-	}
-	lenString += n;
-	newString = (char *)malloc(sizeof(char) * (lenString + 1));
 
-	if (newString == NULL)
-	{
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	}
-	for (index = 0; s1[index]; index++)
-	{
-		newString[index] = s1[index];
-	}
-	for (index1 = 0; s2[index1] && index1 < n; index1++)
-	{
-		newString[index++] = s2[index1];
-	}
-	newString[index] = '\0';
 
-	return (newString);
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
+
