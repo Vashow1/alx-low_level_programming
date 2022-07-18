331103169debd213c4564d17c220bbde45b15f57
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-18 22:53:47 +0300

    Fixing errors in free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tempNode;
 
+	if(head == NULL)
+	{
+		return;
+	}
 	while(*head)
 	{
 		tempNode = *head;
 		*head = (*head)->next;
 		free(tempNode);
 	}
+
+	head = NULL;
 }
