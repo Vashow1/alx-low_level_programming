4389cb561ec221bad7ddf19d1262cd3272b65c4e
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-18 09:41:01 +0300

    Fixing string_nconcat file errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,33 +13,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	char *newString;
+	unsigned int index, index1, lenString;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
+	}
 	for (index = 0; s1[index]; index++)
-		len++;
+	{
+		lenString++;
+	}
+	lenString += n;
+	newString = (char *)malloc(sizeof(char) * (lenString + 1));
 
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
+	if (newString == NULL)
+	{
 		return (NULL);
-
-	len = 0;
-
+	}
 	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
+	{
+		newString[index] = s1[index];
+	}
+	for (index1 = 0; s2[index1] && index1 < n; index1++)
+	{
+		newString[index++] = s2[index1];
+	}
+	newString[index] = '\0';
 
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+	return (newString);
 }
-
